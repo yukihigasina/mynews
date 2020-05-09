bb1bc536ef03b1744671a0a4013a1bf9a8f82f7d
”名前” <>
2020-05-09 04:45:25 +0000

    kadai10

---
+++ resources/views/admin/profile/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>kadai10</title>
+    </head>
+    <body>
+        <hi>いらっしゃい</hi>
+    </body>
+</html>
